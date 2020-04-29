1effae210b355bff443cc949e77068b173b56105
Mark Goldberg <markgoldberg182@gmail.com>
2020-04-29 19:49:05 -0400

    Adding in Vuetify and getting the navbar to match the reddit one

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,68 @@
 <template>
-  <div class="pure-menu pure-menu-horizontal">
-    <div class="container">
-      <router-link to="/" class="pure-menu-heading pure-menu-link">Home</router-link>
-      <ul class="pure-menu-list">
-        <li class="pure-menu-item">
-          <router-link class="pure-menu-link" to="/about">About</router-link>
-        </li>
-      </ul>
-    </div>
-  </div>
+  <v-app-bar
+    app
+    color="primary"
+    dark
+  >
+    <span>Logo</span>
+    <v-text-field
+      solo-inverted
+      flat
+      hide-details
+      label="Search"
+    />
+
+    <v-spacer />
+    <v-menu
+      v-model="value"
+      :disabled="disabled"
+      :absolute="absolute"
+      :open-on-hover="openOnHover"
+      :close-on-click="closeOnClick"
+      :close-on-content-click="closeOnContentClick"
+      :offset-x="offsetX"
+      :offset-y="offsetY"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn
+          color="primary"
+          dark
+          v-on="on"
+        >
+          Username
+        </v-btn>
+      </template>
+      <v-list>
+        <v-list-item
+          v-for="(item, index) in items"
+          :key="index"
+        >
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+  </v-app-bar>
 </template>
 
 <script>
 export default {
-  name: "Navbar"
+  name: "Navbar",
+  data: () => ({
+    items: [
+      { title: 'My Profile' },
+      { title: 'User Settings' },
+      { title: 'Night Mode' },
+      { title: 'Reddit Coins' },
+    ],
+    disabled: false,
+    absolute: false,
+    openOnHover: false,
+    value: false,
+    closeOnClick: true,
+    closeOnContentClick: true,
+    offsetX: false,
+    offsetY: true,
+  })
 }
 </script>
 
