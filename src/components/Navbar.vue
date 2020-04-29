676dba7cc20a10861637afbc4c8f2836ae2e39c6
Mark Goldberg <markgoldberg182@gmail.com>
2020-04-29 19:23:13 -0400

    Initial Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="pure-menu pure-menu-horizontal">
+    <div class="container">
+      <router-link to="/" class="pure-menu-heading pure-menu-link">Home</router-link>
+      <ul class="pure-menu-list">
+        <li class="pure-menu-item">
+          <router-link class="pure-menu-link" to="/about">About</router-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Navbar"
+}
+</script>
+
+<style scoped>
+  .pure-menu {
+    background-color: #ffffff;
+  }
+  .pure-menu a {
+    color: 000000;
+  }
+</style>
