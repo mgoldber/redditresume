72aa8c4bfd7f319a52fd7c611d41d4415f0bc41b
Mark Goldberg <markgoldberg182@gmail.com>
2021-03-28 14:53:50 -0400

    Fixing the sorting issue on the jobs page

--- src/store/actions.js
+++ src/store/actions.js
@@ -1,20 +1,22 @@
 import axios from 'axios';
 
-const url = process.env.VUE_APP_API_URL
+const url = process.env.VUE_APP_API_URL;
 export default {
   fetch_top_posts: ({ commit }) => {
     axios
       .get(`${url}api/v1/post/top`)
       .then(resp => {
         // posts should already be sorted by vote, if not fix here
-        resp.data = resp.data.sort(function(a, b){return b['votes'] - a['votes']})
+        resp.data = resp.data.sort(function(a, b) {
+          return b['votes'] - a['votes'];
+        });
         resp.data.forEach(post => {
           commit('APPEND_TOP_POST', post);
         });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   },
   fetch_all_subreddits: ({ commit }) => {
     axios
@@ -22,25 +24,30 @@ export default {
       .then(resp => {
         resp.data.forEach(subreddit => {
           // Capitalize the first letter of the subreddit name
-          subreddit.name = subreddit.name.charAt(0).toUpperCase() + subreddit.name.slice(1)
+          subreddit.name =
+            subreddit.name.charAt(0).toUpperCase() + subreddit.name.slice(1);
           commit('APPEND_SUBREDDIT', subreddit);
-        })
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   },
   fetch_posts_for_jobs: ({ commit }, subredditId) => {
     axios
       .get(`${url}api/v1/post/subreddit/${subredditId.subredditId}`)
       .then(resp => {
+        // ensure that the posts are sorted by votes
+        resp.data = resp.data.sort(function(a, b) {
+          return b['votes'] - a['votes'];
+        });
         resp.data.forEach(post => {
           commit('APPEND_JOB_POST', post);
-        })
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   },
   fetch_posts_for_projects: ({ commit }, subredditId) => {
     axios
@@ -48,11 +55,11 @@ export default {
       .then(resp => {
         resp.data.forEach(post => {
           commit('APPEND_PROJECTS_POST', post);
-        })
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   },
   fetch_posts_for_ama: ({ commit }, subredditId) => {
     axios
@@ -60,11 +67,11 @@ export default {
       .then(resp => {
         resp.data.forEach(post => {
           commit('APPEND_AMA_POST', post);
-        })
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   },
   send_comment_form: ({ commit }, comment) => {
     axios
@@ -73,10 +80,13 @@ export default {
         comment: comment.comment
       })
       .then(() => {
-        commit('APPEND_SUBMITTED_COMMENT', { name: comment.name, comment: comment.comment });
+        commit('APPEND_SUBMITTED_COMMENT', {
+          name: comment.name,
+          comment: comment.comment
+        });
       })
       .catch(err => {
         console.log(err);
-      })
+      });
   }
 };
